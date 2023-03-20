bc295ad6b37dde6adec71b7dc8a45f98a8eb829c
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2023-03-20 20:56:01 +0000

    started interest rate formula in script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+   //get input values values 
+    let x = document.getElementById("myBalance").value;
+    let y = document.getElementById("myInterest").value;
+    let z = document.getElementById("myDays").value;
+
+
+    //calculate interest on click 
+    let output = 
+
+    document.getElementById("Output").innerHTML = output
+
+    
+}
